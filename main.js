e1fecaf0342909ee3be7a475440a4858eda56cec
Tulqinboyev-Ziyodulla <ziyodulla464@gmail.com>
2024-06-06 23:05:02 +0500

    js

---
+++ main.js
@@ -0,0 +1,98 @@
+alert("Hush kelibsiz")
+
+{
+    // // masala-1
+    // let x = prompt("x")
+
+    // if(x > 0) {
+    //     x++
+    //     console.log(x);
+    // }else{
+    //     console.log(x);
+    // }
+    // alert(`${x}`)
+}
+
+
+{
+    // // masala-2 
+    // let x = prompt("x")
+
+    // if(x > 0) {
+    //     x++
+    //     console.log(x);
+    // }else {
+    //     x = x - 2
+    //     console.log(x);
+    // }
+    // alert(`${x}`)
+}
+
+
+{
+    // // masala-3 //
+    // let x = prompt("x")
+
+    // if(x > 0) {
+    //     x++
+    //     console.log(x);
+    // }else{
+    //     x = x - 2
+    //     console.log(x);
+    // }
+    // alert(`${x}`)
+}
+
+{
+    // // masala-4
+    // let x1 = 5
+    // let x2 = 5
+    // let x3 = -5
+    // let musbat = 0
+
+    // if(x1 > 0) {
+    //     musbat++
+    // }
+    // if(x2 > 0) {
+    //     musbat++
+    // }
+    // if(x3 > 0) {
+    //     musbat++
+    // }
+
+    // console.log(musbat);
+    // alert(`${musbat}`)
+}
+
+{
+    // // masala-5
+    // let x1 = 5
+    // let x2 = 9
+    // let x3 = -8
+    // let musbat = 0
+    // let manfiy = 0
+
+    // if(x1 > 0) {
+    //     musbat++
+    // }
+    // if(x2 > 0) {
+    //     musbat++
+    // }
+    // if(x3 > 0) {
+    //     musbat++
+    // }
+
+    // if(x1 < 0) {
+    //     x1 = x1 - 1
+    // }
+    // if(x2 < 0) {
+    //     x1 = x1 - 1
+    // }
+    // if(x3 < 0) {
+    //     x1 = x1 - 1
+    // }
+
+    // console.log(musbat);
+    // console.log(manfiy);
+    // alert(`${musbat} ${manfiy}`)
+}
