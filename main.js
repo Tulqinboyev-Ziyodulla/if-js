d7c45b2089cd1caa91ace1d24bac6fe9cb243333
Tulqinboyev-Ziyodulla <ziyodulla464@gmail.com>
2024-06-07 09:28:50 +0500

    masala

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ alert("Hush kelibsiz")
 
 {
     // // masala-1
-    // let x = prompt("x")
+    // let x = +prompt("x ga son kiriting")
 
     // if(x > 0) {
     //     x++
@@ -16,7 +16,7 @@ alert("Hush kelibsiz")
 
 {
     // // masala-2 
-    // let x = prompt("x")
+    // let x = +prompt("x ga son kiritng")
 
     // if(x > 0) {
     //     x++
@@ -31,7 +31,7 @@ alert("Hush kelibsiz")
 
 {
     // // masala-3 //
-    // let x = prompt("x")
+    // let x = +prompt("x ga son kiriting")
 
     // if(x > 0) {
     //     x++
@@ -45,9 +45,9 @@ alert("Hush kelibsiz")
 
 {
     // // masala-4
-    // let x1 = 5
-    // let x2 = 5
-    // let x3 = -5
+    // let x1 = +prompt("x1 ga son kirting")
+    // let x2 = +prompt("x1 ga son kirting")
+    // let x3 = +prompt("x1 ga son kirting")
     // let musbat = 0
 
     // if(x1 > 0) {
@@ -64,11 +64,12 @@ alert("Hush kelibsiz")
     // alert(`${musbat}`)
 }
 
+
 {
     // // masala-5
-    // let x1 = 5
-    // let x2 = 9
-    // let x3 = -8
+    // let x1 = +prompt("x1 ga son kirting")
+    // let x2 = +prompt("x2 ga son kirting")
+    // let x3 = +prompt("x3 ga son kirting")
     // let musbat = 0
     // let manfiy = 0
 
@@ -96,3 +97,65 @@ alert("Hush kelibsiz")
     // console.log(manfiy);
     // alert(`${musbat} ${manfiy}`)
 }
+
+// {
+//     // masala 6
+//     let a = +prompt("a sonini kirting")
+//     let b = +prompt("b sonini kiritng")
+//     if(a > b){
+//         alert(`a soni katta: ${b}`)
+//     }else if(a < b){
+//         alert(` b soni katta: ${b}`)
+//     }
+// }
+
+
+// {
+//     // masala 7
+//     let x1 = +prompt("x1 sonini kirting")
+//     let x2 = +prompt("x2 sonini kirting")
+//     if(x1 > x2){
+//         alert(`x2 kichik`)
+//     }else if(x1 < x2){
+//         alert(`x1 kichik`)
+//     }
+// }
+
+
+// {
+//     // masala 8
+//     let x1 = +prompt("x1 sonini kirting")
+//     let x2 = +prompt("x2 sonini kirting")
+//     if(x1 > x2){
+//         alert (`kattasi:${x1} kichigi: ${x2}`)
+//     }else {
+//         alert (`kattasi: ${x2} kichigi: ${x1}`)
+//     }
+// }
+
+
+// {
+//     // masala 9
+//     let A = +prompt("A sonini kirting")
+//     let B = +prompt("B sonini kirting")
+//     if(A < B){
+//         alert (`${A} shu son ${B} dan kichik`)
+//     }
+// }
+
+
+// {
+//     // masala 10
+//     let a = +prompt("a sonini kirting")
+//     let b = +prompt("b sonini kirting")
+//     let sum = 0
+//     if(a!=b){
+//         sum = a + b
+//         alert(`${sum}`)
+//     }else{
+//         a = 0
+//         b = 0
+//         alert(`${a} ${b}`)
+//     }
+    
+// }
